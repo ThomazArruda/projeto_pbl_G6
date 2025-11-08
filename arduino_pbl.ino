2167e9e5eb49378a42be55de3d67540103498367
ThomazArruda <thomazapsouza@gmail.com>
2025-11-07 23:51:59 -0300

    Add files via upload

---
+++ arduino_pbl.ino
@@ -0,0 +1,24 @@
+#define ECG_PIN 35   // GPIO35 para ECG
+#define EMG_PIN 34   // GPIO34 para EMG
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+  Serial.println("--- Leitura de 1 ECG e 1 EMG ---");
+  
+  pinMode(ECG_PIN, INPUT);
+  pinMode(EMG_PIN, INPUT);
+}
+
+void loop() {
+  int valor_ecg = analogRead(ECG_PIN);
+  int valor_emg = analogRead(EMG_PIN);
+
+  // Envia os 2 sinais pela serial
+  Serial.print("ECG: ");
+  Serial.print(valor_ecg);
+  Serial.print(", EMG: ");
+  Serial.println(valor_emg);
+
+  delay(10); // Atraso para evitar sobrecarga no serial
+}
